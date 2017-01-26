3f4f904b45adec4f674681d62c1bb9e9ec2a4b92
Justin G <email": null>
2017-01-25 20:32:06 -0800

    add routing tests for remaining robot routes

--- spec/routing/robot_routing_spec.rb
+++ spec/routing/robot_routing_spec.rb
@@ -5,4 +5,23 @@ RSpec.describe "Routing to robots", :type => :routing do
     expect(:get => "/robots").to route_to("robots#index")
   end
 
+  it "routes GET /robots/new to robots#new" do
+    expect(:get => "/robots/new").to route_to("robots#new")
+  end
+
+  it "routes GET /robots/1 to robots#show" do
+    expect(:get => "/robots/1").to route_to("robots#show", :id => "1")
+  end
+
+  it "routes GET /robots/1/edit to robots#edit" do
+    expect(:get => "/robots/1/edit").to route_to("robots#edit", :id => "1")
+  end
+
+  it "routes PUT /robots1 to robots#update" do
+    expect(:put => "/robots/1").to route_to("robots#update", :id => "1")
+  end
+
+  it "routes POST /robots to robots#create" do
+    expect(:post => "/robots").to route_to("robots#create")
+  end
 end
