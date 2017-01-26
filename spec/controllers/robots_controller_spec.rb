fc5568e6f399d1ef8bbcb3c4614b83e1adfaaba5
Justin G <email": null>
2017-01-25 20:40:06 -0800

    create tests for robot controller to #index

--- spec/controllers/robots_controller_spec.rb
+++ spec/controllers/robots_controller_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe RobotsController, type: :controller do
+  let!(:robot) { FactoryGirl.create(:robot) }
+
+  describe "GET #index" do
+    it "responds with status code 200" do
+      get :index
+      expect(response).to have_http_status 200
+    end
+
+    it "assigns all robots as @robots" do
+      get :index
+      expect(assigns(:robots)).to eq Robot.all
+    end
+
+    it "renders the :index template" do
+      get :index
+      expect(response).to render_template :index
+    end
+  end
 
 end
