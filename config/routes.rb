48b02e357727b390a1f8620bc893ed09a69070d9
Katherine Guenther <guenther@peak.org>
2017-01-25 18:16:39 -0800

    Remove a few routes from manufacturers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :robots, except: [:destroy]
 
-  resources :manufacturers do
+  resources :manufacturers, only: [:index, :show] do
     resources :products
   end
 end
