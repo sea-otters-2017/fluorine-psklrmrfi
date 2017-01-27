00194c44d40355d4beed9ec3740cc2c815b98c58
Katherine Guenther <guenther@peak.org>
2017-01-26 16:39:59 -0800

    Add products controller and tests

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -6,34 +6,34 @@ RSpec.describe ProductsController, type: :controller do
 
   describe "GET #index" do
     it "responds with status code 200" do
-      get :index
+      get :index, params: { manufacturer_id: manufacturer.id }
       expect(response).to have_http_status 200
     end
 
     it "assigns all products as @products" do
-      get :index
-      expect(assigns(:products)).to eq product.all
+      get :index, params: { manufacturer_id: manufacturer.id }
+      expect(assigns(:products)).to eq Product.all
     end
 
     it "renders the :index template" do
-      get :index
+      get :index, params: { manufacturer_id: manufacturer.id }
       expect(response).to render_template :index
     end
   end
 
   describe "GET #show" do
     it "responds with status code 200" do
-      get :show, params: { id: product.id }
+      get :show, params: { manufacturer_id: manufacturer.id, id: product.id}
       expect(response).to have_http_status 200
     end
 
     it "assigns the correct product as @product" do
-      get :show, params: { id: product.id }
+      get :show, params: { manufacturer_id: manufacturer.id, id: product.id}
       expect(assigns(:product)).to eq(product)
     end
 
     it "renders the :show template" do
-      get :show, params: { id: product.id }
+      get :show, params: { manufacturer_id: manufacturer.id, id: product.id}
       expect(response).to render_template(:show)
     end
   end
