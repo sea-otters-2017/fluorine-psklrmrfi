fb256c1fc86566a3e87c6e2db10a40215df19ffb
Kate Kourbatova & Pat Holland <pair+katekourbatova.pholls@devbootcamp.com>
2017-01-26 16:09:09 -0800

    Sessions helpers, login/logout routes, login link in layout

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,11 +1,16 @@
 module SessionsHelper
+
   def set_user(user)
-    session[user_id] = user.id
+    session[:user_id] = user.id
   end
 
   def user_logged_in?
-    return true if session[user_id] == User.first.id #The user is the only user
+    return true if session[:user_id] == User.first.id #The user is the only user
     false
+  end
+
+  def logout
+    session[:user_id] = nil
   end
 
 end
