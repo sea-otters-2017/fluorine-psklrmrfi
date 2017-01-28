e5d48955202c970015731ea588545ceab3421e68
Justin G <email": null>
2017-01-27 19:13:05 -0800

    update the product specs to include price

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   describe 'validations' do
     let!(:manufacturer) { Manufacturer.create!(name: 'Acme Robots') }
-    let!(:product) { Product.create!(model_number: 'XY-123', height: 40, weight: 80, manufacturer_id: manufacturer.id ) }
+    let!(:product) { Product.create!(model_number: 'XY-123', height: 40, weight: 80, manufacturer_id: manufacturer.id, price: 95000 ) }
 
     it 'should have a model number' do
       expect(product).to validate_presence_of :model_number
