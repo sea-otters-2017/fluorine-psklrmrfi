70f04c0341bcb379983a6f5a563945ddc07f8eee
Justin G <just.grasse@gmail.com>
2017-01-27 19:35:00 -0800

    provide adequate spaces for division operations

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,9 +1,9 @@
 module ProductsHelper
   def display_price(price)
     if session[:admin]
-      return price/ 100
+      return price / 100
     else
-      return (price/ 100) * 2
+      return (price / 100) * 2
     end
   end
 end
