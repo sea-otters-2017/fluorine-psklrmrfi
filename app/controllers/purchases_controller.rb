e842d3bc967dda6bdf35b8e6dca9d8c9d4610d08
Pat Holland <email": null>
2017-01-27 16:54:35 -0800

    Create purchases controller

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,22 @@
+class PurchasesController < ApplicationController
+  def new
+    # do I need this?
+    # form partial appears on Robot show page
+  end
+
+  def create
+    purchase = Purchase.new(purchase_params)
+    if purchase.save
+      flash[:notice] = 'Your robot has been purchased.'
+      redirect_to robots_path
+    else
+      #re-render that robot page
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require[:robot].permit[:name, :email]
+  end
+end
