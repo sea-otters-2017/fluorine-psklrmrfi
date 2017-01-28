448cfb76598b482561e0fafbb804917337f136e0
Pat Holland <email": null>
2017-01-27 20:21:35 -0800

    Migrate db and update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170128010948) do
+ActiveRecord::Schema.define(version: 20170128040620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20170128010948) do
     t.datetime "created_at",                    null: false
     t.datetime "updated_at",                    null: false
     t.datetime "expected_date"
+    t.boolean  "available",     default: false
   end
 
   create_table "users", force: :cascade do |t|
