9db04565a78a7762d282d95dc34c95fe2160cdd4
Justin G <email": null>
2017-01-27 18:36:14 -0800

    schema conflict resolved

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170128000919) do
+ActiveRecord::Schema.define(version: 20170128010948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,11 +42,12 @@ ActiveRecord::Schema.define(version: 20170128000919) do
   end
 
   create_table "robots", force: :cascade do |t|
-    t.string   "name",                       null: false
-    t.boolean  "in_stock",   default: false
-    t.integer  "product_id",                 null: false
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
+    t.string   "name",                          null: false
+    t.boolean  "in_stock",      default: false
+    t.integer  "product_id",                    null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+    t.datetime "expected_date"
   end
 
   create_table "users", force: :cascade do |t|
