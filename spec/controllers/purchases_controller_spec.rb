ec0e4ca6228d0f3d3158ec6e8f629e708ddc9916
Pat Holland <email": null>
2017-01-28 15:54:24 -0800

    Attempting purchases controller specs

--- spec/controllers/purchases_controller_spec.rb
+++ spec/controllers/purchases_controller_spec.rb
@@ -2,20 +2,22 @@ require 'rails_helper'
 
 RSpec.describe PurchasesController do
   let!(:robot) { FactoryGirl.create(:robot) }
+  let!(:new_purchase) { FactoryGirl.build(:purchase) }
 
-  # describe 'POST #create' do
-  #   it 'saves the purchase with valid params' do
-  #     purchase_params = FactoryGirl.attributes_for(:purchase)
-  #     purchase_params[:robot_id] = robot.id
-  #     count = Purchase.count
-  #     post :create, purchase: purchase_params
-  #     expect(Purhcase.count).to eq(count + 1)
-  #   end
+  describe 'POST #create' do
+    let(:successful_post) do
+      post :create, params: { robot_id: robot.id, purchase: new_purchase.attributes }
+    end
 
-  #   it 'redirects to robot_show_page if purchase does not save' do
-  #     post :create, robot_id: robot.id
-  #     expect(response).to redirect_to robot_path(robot)
-  #   end
-  # end
+    it 'responds 302 for a successful post' do
+      successful_post
+      expect(response).to have_http_status(302)
+    end
+
+    it 'redirects to robot_show_page if purchase does not save' do
+      post :create, robot_id: robot.id
+      expect(response).to redirect_to robot_path(robot)
+    end
+  end
 
 end
