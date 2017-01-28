b52fdd7281022d1afc7a24b2ac08d6216607de74
Justin G <just.grasse@gmail.com>
2017-01-27 16:03:30 -0800

    commented out robot api js file - WIP

--- app/assets/javascripts/robot_api.js
+++ app/assets/javascripts/robot_api.js
@@ -1,23 +1,23 @@
-$( document ).ready(function() {
+// $( document ).ready(function() {
 
-  function getUpdates() {
-    var xmlhttp = new XMLHttpRequest()
-    var url = 'http://jordankamin.com/robots_api/robots.json'
-    alert('Getting updates')
+//   function getUpdates() {
+//     var xmlhttp = new XMLHttpRequest()
+//     var url = 'http://jordankamin.com/robots_api/robots.json'
+//     alert('Getting updates')
 
-    xmlhttp.onreadystatechange = function() {
-      if (xmlhttp.readyState == XMLHttpRequest.DONE) {
-          alert(xmlhttp.responseText)
-      }
-    }
+//     xmlhttp.onreadystatechange = function() {
+//       if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+//           alert(xmlhttp.responseText)
+//       }
+//     }
 
-    xmlhttp.open('GET', url, true)
-    xmlhttp.send();
-  };
+//     xmlhttp.open('GET', url, true)
+//     xmlhttp.send();
+//   };
 
-  // setInterval(getUpdates, 10000)
+//   // setInterval(getUpdates, 10000)
 
-});
+// });
 
 
 
