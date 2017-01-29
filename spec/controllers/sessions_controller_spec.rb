93b2f4538207fc5593ebb62ec7ed961a2e92a1f0
Pat Holland <email": null>
2017-01-28 17:06:41 -0800

    Session controller spec
    * Tests that logout destroys admin and user session
    * Tests that DELETE#destroy redirects properly
    * Still working on POST#create

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -11,23 +11,33 @@ RSpec.describe SessionsController do
   end
 
   describe 'POST #create' do
-    it 'displays the login page' do
-      controller.session[:user_id] = user.id
-      p controller.params
-      post :create, session: { user_id: user.id}
-      expect(response).to have_http_status 200
+    describe 'successful login' do
+      it 'displays the login page' do
+        p controller.params
+        post :create, session: { user_id: user.id}
+        expect(response).to have_http_status 200
+      end
     end
   end
 
   describe 'DELETE #destroy' do
-    it 'redirects to the unwelcome page' do
+    it 'redirects to the unwelcome page (status code)' do
+      controller.session[:user_id] = user.id
       delete :destroy
       expect(response).to have_http_status 302
     end
 
-    it 'redirects to the unwelcome page' do
+    it 'redirects to the unwelcome page (redirect_to)' do
+      controller.session[:user_id] = user.id
       delete :destroy
       expect(response).to redirect_to root_path
+    end
+
+    it 'destroys the session' do
+      controller.session[:user_id] = user.id
+      delete :destroy
+      expect(controller.session[:admin]).to be false
+      expect(controller.session[:user_id]).to be nil
     end
   end
 end
