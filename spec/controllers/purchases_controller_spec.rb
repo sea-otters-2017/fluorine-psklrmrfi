5c88105ddb26e14271541e4e04e3430a49dd6ee3
Pat Holland <email": null>
2017-01-28 16:27:56 -0800

    Cover purchases controller with specs

--- spec/controllers/purchases_controller_spec.rb
+++ spec/controllers/purchases_controller_spec.rb
@@ -9,15 +9,29 @@ RSpec.describe PurchasesController do
       post :create, params: { robot_id: robot.id, purchase: new_purchase.attributes }
     end
 
+    let(:unsuccessful_post) do
+      post :create, params: { robot_id: robot.id, purchase: {name: new_purchase.name} }
+    end
+
     it 'responds 302 for a successful post' do
       successful_post
       expect(response).to have_http_status(302)
     end
 
-    it 'redirects to robot_show_page if purchase does not save' do
-      post :create, robot_id: robot.id
-      expect(response).to redirect_to robot_path(robot)
+    describe 'for a unsuccessful post' do
+      it "responds with status code 200" do
+        unsuccessful_post
+        expect(response).to have_http_status 302
+      end
+
+      it "does not create a new purchase" do
+        expect{unsuccessful_post}.to_not change(Purchase, :count)
+      end
+
+      it "re-displays the same robot" do
+        unsuccessful_post
+        expect(response).to redirect_to robot_path(robot)
+      end
     end
   end
-
 end
