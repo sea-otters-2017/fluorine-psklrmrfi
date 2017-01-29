9ce98502a0c82cd7d9968ddddec2c44a9a7f64be
Pat Holland <email": null>
2017-01-28 16:54:25 -0800

    Sessions Controller Specs
    * One test still failing
    * Working on verifying POST#create route

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -3,4 +3,31 @@ require 'rails_helper'
 RSpec.describe SessionsController do
   let!(:user) { FactoryGirl.create(:user) }
 
+  describe 'GET #new' do
+    it 'displays the login page' do
+      get :new
+      expect(response).to have_http_status 200
+    end
+  end
+
+  describe 'POST #create' do
+    it 'displays the login page' do
+      controller.session[:user_id] = user.id
+      p controller.params
+      post :create, session: { user_id: user.id}
+      expect(response).to have_http_status 200
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    it 'redirects to the unwelcome page' do
+      delete :destroy
+      expect(response).to have_http_status 302
+    end
+
+    it 'redirects to the unwelcome page' do
+      delete :destroy
+      expect(response).to redirect_to root_path
+    end
+  end
 end
